dfdc811b0780b471013c2a8bfb2a5bc14a9d4714
RuntimeErrorX <kholder2@gmail.com>
2021-06-03 23:40:53 -0400

    new commit

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -22,7 +22,7 @@ export const lightColors: Colors = {
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
-  tertiary: "#EFF4F5",
+  tertiary: "#4267B2",
   text: "#452A7A",
   textDisabled: "#BDC2C4",
   textSubtle: "#4267B2",
