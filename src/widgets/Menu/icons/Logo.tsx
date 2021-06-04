abd2bfe5689e2d4eb37f928aa7cbd674b7cb8e5c
RuntimeErrorX <kholder2@gmail.com>
2021-06-04 04:21:09 -0400

    new commit

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -9,8 +9,8 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 205 26" {...props}>
-      <image width="200" height="70" href={isDark ? '/images/egg/20.png' : '/images/egg/20.png'}/>
+    <Svg viewBox="0 0 170 65" {...props}>
+      <image width="150" height="60" href={isDark ? '/images/egg/20.png' : '/images/egg/20.png'}/>
     </Svg>
   )
 };
