4e5d6ead3bff9d0b205816ffdaeb52309ff1e256
RuntimeErrorX <kholder2@gmail.com>
2021-06-04 22:50:05 -0400

    new commit

--- src/widgets/Menu/types.ts
+++ src/widgets/Menu/types.ts
@@ -42,6 +42,7 @@ export interface PanelProps {
   isDark: boolean;
   toggleTheme: (isDark: boolean) => void;
   cakePriceUsd?: number;
+  cakePrice2Usd?: number;
   currentLang: string;
   langs: LangType[];
   setLang: (lang: LangType) => void;
